876f4f790c01183ca81b3ed305dd181fd9ce2258
Thomas Brochard <thomasbrochard.com@gmail.com>
2024-06-29 06:26:24 +0200

    final commit

--- lightbox.php
+++ lightbox.php
@@ -1,4 +1,11 @@
 <!-- Modale Lightbox -->
+<?php
+        $galerie = get_post(); // Assuming you want the current post object
+        $reference = get_field('reference_photo');
+        $categories = get_the_terms(get_the_ID(), 'categories_photo');
+        
+
+        ?>
 <div class="lightbox" id="lightbox-container">
   <div class="lightbox__content">
     <button class="lightbox__close btn-close" id="close-lightbox" type="button">
@@ -9,13 +16,20 @@
                 <img class="arrow arrowNavL" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/navArrowL.png" alt="nav arrow left"/>
                 <span>Précédente</span>
             </button>
+            <div>
     <div class="lightbox__image">
       <!-- L'image en plein écran sera injectée ici via JavaScript -->
+    </div>
+        <div class="img-infos-lightbox">
+          <p><?php echo get_field('reference_photo'); ?></p>
+          <p><?php echo strip_tags(get_the_term_list($post->ID, 'categories_photo')); ?></p>
+        </div>
     </div>
             <button class="lightbox_next">
                 <span>Suivante</span>
                 <img class="arrow arrowNavR" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/navArrowR.png" alt="nav arrow right"/>
             </button>
         </div>
+
   </div>
 </div>
